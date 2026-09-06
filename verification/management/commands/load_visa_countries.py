ee70dfdfcf11d5828a3a607a4d8f366ff5eb9e5b
jibon7200 <jibon.mp@gmail.com>
2026-09-06 16:37:44 +0600

    Add 8 search-verified visa checking countries (GCC + Malaysia + Singapore)

--- verification/management/commands/load_visa_countries.py
+++ verification/management/commands/load_visa_countries.py
@@ -3,43 +3,82 @@ from verification.models import VerificationConfig
 
 
 class Command(BaseCommand):
-    help = 'Loads a starter set of VERIFIED visa verification configurations for common destination countries.'
+    help = 'Loads a starter set of VERIFIED visa verification configurations for key destination countries for Bangladeshi workers/travelers.'
 
     def handle(self, *args, **options):
         countries = [
             {
                 'country_or_provider': 'Saudi Arabia',
-                'service_type': VerificationConfig.ServiceType.VISA,
                 'required_fields': [
                     {'name': 'visa_or_application_number', 'label': 'Visa / Application Number'},
                     {'name': 'passport_number', 'label': 'Passport Number'},
                 ],
-                'method': VerificationConfig.Method.WEBSITE,
                 'official_url': 'https://visa.mofa.gov.sa/',
-                'instructions': 'Verified via MOFA visa portal (visa.mofa.gov.sa). Enjaz has been merged into MOFA.',
+                'instructions': 'MOFA visa portal. Enjaz (enjazit.com.sa) has been fully merged into MOFA.',
             },
             {
                 'country_or_provider': 'United Arab Emirates (UAE)',
-                'service_type': VerificationConfig.ServiceType.VISA,
                 'required_fields': [
                     {'name': 'passport_number', 'label': 'Passport Number'},
                     {'name': 'passport_expiry_date', 'label': 'Passport Expiry Date'},
                     {'name': 'nationality', 'label': 'Nationality'},
                 ],
-                'method': VerificationConfig.Method.WEBSITE,
                 'official_url': 'https://smartservices.icp.gov.ae/',
-                'instructions': 'ICP Smart Services covers all Emirates except Dubai. For Dubai-issued visas, use GDRFA Dubai instead.',
+                'instructions': 'ICP Smart Services covers all Emirates except Dubai. Dubai-issued visas use GDRFA Dubai instead.',
             },
             {
                 'country_or_provider': 'Qatar',
-                'service_type': VerificationConfig.ServiceType.VISA,
                 'required_fields': [
                     {'name': 'visa_or_qid_number', 'label': 'Visa Number / QID Number'},
                     {'name': 'passport_number', 'label': 'Passport Number'},
                 ],
-                'method': VerificationConfig.Method.WEBSITE,
                 'official_url': 'https://portal.moi.gov.qa/',
                 'instructions': 'Ministry of Interior (MOI) e-services portal. Also available via the Metrash2 mobile app.',
+            },
+            {
+                'country_or_provider': 'Oman',
+                'required_fields': [
+                    {'name': 'application_number', 'label': 'Web Application Number'},
+                    {'name': 'passport_number', 'label': 'Passport Number'},
+                ],
+                'official_url': 'https://evisa.rop.gov.om/',
+                'instructions': 'Royal Oman Police (ROP) eVisa portal.',
+            },
+            {
+                'country_or_provider': 'Kuwait',
+                'required_fields': [
+                    {'name': 'evisa_reference_number', 'label': 'eVisa Reference Number'},
+                    {'name': 'passport_number', 'label': 'Passport Number'},
+                ],
+                'official_url': 'https://evisa.moi.gov.kw/',
+                'instructions': 'Kuwait Ministry of Interior (MOI) e-Visa portal.',
+            },
+            {
+                'country_or_provider': 'Bahrain',
+                'required_fields': [
+                    {'name': 'application_reference_number', 'label': 'Application Reference Number'},
+                    {'name': 'passport_number', 'label': 'Passport Number'},
+                ],
+                'official_url': 'https://www.evisa.gov.bh/',
+                'instructions': 'Managed by Nationality, Passports and Residence Affairs (NPRA).',
+            },
+            {
+                'country_or_provider': 'Malaysia',
+                'required_fields': [
+                    {'name': 'passport_number', 'label': 'Passport Number'},
+                    {'name': 'sticker_number', 'label': 'Sticker / Approval Number'},
+                ],
+                'official_url': 'https://malaysiavisa.imi.gov.my/evisa/evisa.jsp',
+                'instructions': 'Official MYVISA portal by the Immigration Department of Malaysia. Beware of unofficial third-party sites.',
+            },
+            {
+                'country_or_provider': 'Singapore',
+                'required_fields': [
+                    {'name': 'passport_number', 'label': 'Passport Number'},
+                    {'name': 'application_reference_number', 'label': 'Application Reference Number'},
+                ],
+                'official_url': 'https://www.ica.gov.sg/',
+                'instructions': 'Immigration and Checkpoints Authority (ICA) of Singapore.',
             },
         ]
 
@@ -47,10 +86,10 @@ class Command(BaseCommand):
         for entry in countries:
             obj, created = VerificationConfig.objects.get_or_create(
                 country_or_provider=entry['country_or_provider'],
-                service_type=entry['service_type'],
+                service_type=VerificationConfig.ServiceType.VISA,
                 defaults={
                     'required_fields': entry['required_fields'],
-                    'method': entry['method'],
+                    'method': VerificationConfig.Method.WEBSITE,
                     'official_url': entry['official_url'],
                     'instructions': entry['instructions'],
                 }
